910f61d7c492816a62fd2091c4463f0406444b1e
Shamaratun <shamaratunmeme@gmail.com>
2025-02-17 13:05:52 +0600

    template not correct

--- Forthahw/src/app/app.component.ts
+++ Forthahw/src/app/app.component.ts
@@ -1,51 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import{HeaderComponent} from './header/header.component';
-import{FooterComponent} from'./footer/footer.component';
-// import{ContentComponent}from './content/content.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
-  templateUrl: './app.component.html',
-  // template:`
+  imports: [HeaderComponent, FooterComponent, ContentComponent, RouterOutlet],
+  template: `
+    <app-header></app-header>
+    <h1>Hello {{ title }}</h1>
+    <img [src]="image" alt="Profile Picture" width="100" height="100">
+    <button (mouseover)="showAlert()">Click me</button>
     
-  // `,
+    <p>{{ KeyPressed }}</p>
+  
+    <textarea (keyup)="onKeyUp($event)"></textarea>
+    <button (onClick)="showtext">Click me</button>
+    
+    <app-footer></app-footer>
+  `,
   styles: `
-  .a {
-     background:yellow;
+    .a {
+      background: yellow;
       color: orange;
-      font-size:50px;
-      weidth:150px;
-      height:150px;
-  }`
-  // styleUrl: './app.component.css'
+      font-size: 50px;
+      width: 150px; 
+      height: 150px;
+    }
+  `
 })
 export class AppComponent {
   title = 'Forthahw';
-//   users=[
-//   {
-//   Name:'Meme',
-//   Age:28,
-//   city:'Jashore',
-//   Country:'Bangladesh',
-//   ProfilePicture:"https://placebeard.it/250/250",
-//   },
-//   {
-//   Name:"Omar",
-//   Age:27,
-//   city:'dhaka',
-//   Country:'Bangladesh',
-//   ProfilePicture:"https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+  altImg = 'logo of angular';
+  image = 'https://angular.io/assets/images/logos/angular/angular.svg';
+  KeyPressed = ''; 
+  textfromtextarea='';
+  onKeyUp(event: KeyboardEvent) {
+    this.KeyPressed = (event.target as HTMLInputElement).value;
+  }
 
-//   },
-//   {
-//     Name:"Rasel",
-//   Age:28,
-//   city:'Tangail',
-//   Country:'Bangladesh',
-//   ProfilePicture:"https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk",
-//   },
-// ];
-
+  showAlert() {
+    alert('dummy alert');
+  }
+  showtext(){
+    alert(this.KeyPressed);
+  }
 }
--- Forthahw/src/app/content/content.component.ts
+++ Forthahw/src/app/content/content.component.ts
@@ -4,33 +4,33 @@ import { Component } from '@angular/core';
   selector: 'app-content',
   imports: [],
   templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+  styleUrl: './content.component.css',
 })
 export class ContentComponent {
-
-users=[
-  {
-  Name:'Meme',
-  Age:28,
-  city:'Jashore',
-  Country:'Bangladesh',
-  ProfilePicture:"https://placebeard.it/250/250",
-  },
-  {
-  Name:"Omar",
-  Age:27,
-  city:'dhaka',
-  Country:'Bangladesh',
-  ProfilePicture:"https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
-
-  },
-  {
-    Name:"Rasel",
-  Age:28,
-  city:'Tangail',
-  Country:'Bangladesh',
-  ProfilePicture:"https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk",
-  },
-];
-
+  users = [
+    {
+      Name: 'Meme',
+      Age: 29,
+      city: 'Jashore',
+      Country: 'Bangladesh',
+      ProfilePicture:
+      "https://media.istockphoto.com/id/1500285927/photo/young-woman-a-university-student-studying-online.jpg?s=1024x1024&w=is&k=20&c=CVhpekieDK_UB8vtEDw-dKKGWzDpsxcQt-XEQIkgm3Y=    "
+      },
+    
+    {
+      Name: 'Omar',
+      Age: 27,
+      city: 'Dhaka',
+      Country: 'Bangladesh',
+      ProfilePicture:
+        'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    },
+    {
+      Name: 'Rasel',
+      Age: 28,
+      city: 'Tangail',
+      Country: 'Bangladesh',
+      ProfilePicture: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    },
+  ];
 }
